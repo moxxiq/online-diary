752527d1a3b3feb4e4dc3bd6d1e02a29bbac7611
Moxxiq <dimanavsisto@gmail.com>
2021-11-21 01:19:57 +0200

    Add additional validation

--- services/backend/app/api/notes.py
+++ services/backend/app/api/notes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
 from app.api.models import NoteDB, NoteSchema
@@ -18,18 +18,19 @@ async def create_note(payload: NoteSchema):
     return response_object
 
 @router.get("/{id}/", response_model=NoteDB)
-async def read_note(id: int):
+async def read_note(id: int = Path(..., gt=0),):
     note = await crud.get(id)
     if not note:
         raise HTTPException(status_code=404, detail="Note not found")
     return note
+
 
 @router.get("/", response_model=list[NoteDB])
 async def read_all_notes():
     return await crud.get_all()
 
 @router.put("/{id}/", response_model=NoteDB)
-async def update_note(id: int, payload: NoteSchema):
+async def update_note(payload: NoteSchema, id: int = Path(..., gt=0),):
     note = await crud.get(id)
     if not note:
         raise HTTPException(status_code=404, detail="Note not found")
@@ -42,3 +43,15 @@ async def update_note(id: int, payload: NoteSchema):
         "description": payload.description,
     }
     return response_object
+
+
+@router.delete("/{id}/", response_model=NoteDB)
+async def delete_note(id: int = Path(..., gt=0)):
+    note = await crud.get(id)
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    await crud.delete(id)
+
+    return note
+
