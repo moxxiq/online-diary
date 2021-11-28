e81de816259480186493cd09532267901cdd9453
Moxxiq <dimanavsisto@gmail.com>
2021-11-29 01:59:00 +0200

    Models refactor. Migrations conf

--- services/backend/app/core/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# DO NOT PUT AMY SQLAlchemy MODELS
-# Metadata is needed in all tables to create all of this at once
--- services/backend/app/db.py
+++ services/backend/app/db.py
@@ -1,52 +1,11 @@
-import enum
 from decouple import config
-
-from app.core.schemas.users import UserType
-
-from sqlalchemy import (
-    Column,
-    DateTime,
-    Integer,
-    MetaData,
-    String,
-    Enum,
-    Date,
-    Table,
-    UniqueConstraint,
-    create_engine
-)
-from sqlalchemy.sql import func
-
+from sqlalchemy import create_engine
 from databases import Database
 
 DATABASE_URL = config('DATABASE_URL')
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-notes = Table(
-    "notes_test",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("title", String),
-    Column("description", String),
-    Column("created_date", DateTime, default=func.now(), nullable=False),
-)
-
-users = Table(
-    "users",
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('email', String, unique=True, nullable=False),
-    Column('password', String(64), nullable=False),
-    Column('type', Enum(UserType), nullable=False),
-    Column('name', String, nullable=False),
-    Column('surname', String, nullable=False),
-    Column('midname', String),
-    Column('birthday', Date),
-    UniqueConstraint('name', 'surname', 'midname', 'birthday'),
-)
-
 
 # databases query builder
 database = Database(DATABASE_URL)
