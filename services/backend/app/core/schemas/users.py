ccc84d72d6c76d540903ef70eb1e9547df870d7a
Moxxiq <dimanavsisto@gmail.com>
2021-11-30 00:03:30 +0200

    Fix. Pydantic types validation

--- services/backend/app/core/schemas/users.py
+++ services/backend/app/core/schemas/users.py
@@ -1,12 +1,11 @@
 from datetime import date
-import enum
+from typing import Optional
 
 from pydantic import BaseModel, Field, EmailStr
 
 class User(BaseModel):
     email: EmailStr = Field(...)
-    password: str = Field(...)
-    type: int = Field(...)
+    type: str = Field(...)
     name: str = Field(...)
     surname: str = Field(...)
     midname: str = Field(...)
