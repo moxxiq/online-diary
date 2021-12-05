d35a04dc00c95456ba3b5d97684437430d2da01f
Moxxiq <dimanavsisto@gmail.com>
2021-12-06 01:44:36 +0200

    Fix. Make emails only in lowercase

--- services/backend/app/core/crud/users.py
+++ services/backend/app/core/crud/users.py
@@ -11,12 +11,12 @@ async def get_all(page=None, per_page=None):
     pass
 
 async def get_by_email(email: str):
-    query = users.select().where(email == users.c.email)
+    query = users.select().where(email.lower() == users.c.email)
     return await database.fetch_one(query=query)
 
 async def post(payload: NewUser):
     query = users.insert().values(
-        email=payload.email,
+        email=payload.email.lower(),
         hashed_password=get_password_hash(payload.password),
         type=payload.type,
         name=payload.name,
