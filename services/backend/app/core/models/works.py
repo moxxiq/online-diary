6652210b234aa41f4ddc95e50972606c783b488e
Moxxiq <dimanavsisto@gmail.com>
2021-12-05 02:18:29 +0200

    Add columnt work_types

--- services/backend/app/core/models/works.py
+++ services/backend/app/core/models/works.py
@@ -14,6 +14,7 @@ from sqlalchemy import (
 from sqlalchemy.sql import func
 
 from app.core.models.workplaces import workplaces
+from app.core.models.work_types import work_types
 
 metadata = MetaData()
 
@@ -22,9 +23,11 @@ works = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("workplace_id", Integer, nullable=False),
+    Column("work_type_id", Integer, nullable=False),
     Column("deadline", DateTime),
     Column("headline", DateTime, nullable=False),
     Column("description", DateTime),
     Column("creation_date", DateTime, default=func.now(), nullable=False),
     ForeignKeyConstraint(["workplace_id"], [workplaces.c.id], name="works_workplaces_id_fk", ondelete="CASCADE"),
+    ForeignKeyConstraint(["work_type_id"], [work_types.c.id], name="works_work_types_id_fk", ondelete="CASCADE"),
 )
