08f5b095134746648f3dfcd4e7febeed20e41522
Moxxiq <dimanavsisto@gmail.com>
2021-12-05 02:46:57 +0200

    Fix mark-work FK

--- services/backend/app/core/models/marks.py
+++ services/backend/app/core/models/marks.py
@@ -13,7 +13,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.sql import func
 
-from app.core.models.workplaces import workplaces
+from app.core.models.works import works
 from app.core.models.students import students
 
 metadata = MetaData()
@@ -24,8 +24,8 @@ marks = Table(
     Column("id", Integer, primary_key=True),
     Column("creation_date", DateTime, nullable=False, default=func.now()),
     Column("comment", String),
-    Column("workplace_id", Integer, nullable=False),
+    Column("work_id", Integer, nullable=False),
     Column("student_id", Integer, nullable=False),
-    ForeignKeyConstraint(["workplace_id"], [workplaces.c.id], name="marks_workplace_id_fk", ondelete="CASCADE"),
+    ForeignKeyConstraint(["work_id"], [works.c.id], name="marks_work_id_fk", ondelete="CASCADE"),
     ForeignKeyConstraint(["student_id"], [students.c.user_id], name="marks_student_id_fk", ondelete="CASCADE"),
 )
