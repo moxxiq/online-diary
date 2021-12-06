6d56d4e9392f0ee773ecfd237e78af7927a92a19
Moxxiq <dimanavsisto@gmail.com>
2021-12-06 02:47:00 +0200

    FIX HEROKU LEGACY SHIT

--- services/backend/app/db.py
+++ services/backend/app/db.py
@@ -2,7 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine
 from databases import Database
 
-DATABASE_URL = config('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL_SQL')
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
