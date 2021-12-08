c26c48b2d22ddd8e47e954290ed587222dc372b5
Moxxiq <dimanavsisto@gmail.com>
2021-12-09 01:01:12 +0200

    Add create student endpoint

---
+++ services/backend/app/api/v1/endpoints/students.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status, HTTPException, Path, Depends
+
+import app.core.crud as crud
+from app.core.authorization import get_current_user, get_current_user_with_scopes
+from app.core.schemas.users import User
+from app.core.schemas.students import Student
+
+router = APIRouter()
+
+
+
+@router.post("/", response_model=Student, status_code=status.HTTP_201_CREATED)
+async def create_student(payload: Student, current_user: User = Depends(get_current_user_with_scopes([1]))):
+    student_in_db = await crud.students.get(payload.user_id)
+    if student_in_db:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This user is already a student")
+    await crud.students.post(payload)
+    response_object = await crud.students.get(payload.user_id)
+    return response_object
+
