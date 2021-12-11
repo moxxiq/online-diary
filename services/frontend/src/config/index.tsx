1ec1c8da7fec89b0a7a3be8ba09473df3c07d4bc
Moxxiq <dimanavsisto@gmail.com>
2021-12-11 17:53:31 +0200

    HARDCODE API URI

--- services/frontend/src/config/index.tsx
+++ services/frontend/src/config/index.tsx
@@ -1,2 +1,3 @@
 export const BASE_URL: string = process.env.BASE_URL || 'http://localhost:8001';
-export const BACKEND_URL: string = process.env.BACKEND_URL || 'http://localhost:8002/api/v1';
+export const BACKEND_URL: string = 'https://online-diary-mathape.herokuapp.com/api/v1';
+// export const BACKEND_URL: string = process.env.BACKEND_URL || 'http://localhost:8002/api/v1';
