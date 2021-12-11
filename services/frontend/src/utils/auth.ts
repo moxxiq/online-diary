b2124cb5ffc3747b59b8ae0ac90aae2964bfb3bd
Na3aga <na3aga@gmail.com>
2021-12-11 02:29:20 +0200

    Login. Enhancement

--- services/frontend/src/utils/auth.ts
+++ services/frontend/src/utils/auth.ts
@@ -1,11 +1,13 @@
 import decodeJwt from 'jwt-decode';
+import { BACKEND_URL } from '../config';
 
 export const isAuthenticated = () => {
-  const permissions = localStorage.getItem('permissions');
+  const permissions = localStorage.getItem('token_type');
+  console.log({permissions})
   if (!permissions) {
     return false;
   }
-  return permissions === 'user' || permissions === 'admin' ? true : false;
+  return permissions === 'bearer' || permissions === 'admin' ? true : false;
 };
 
 /**
@@ -26,7 +28,7 @@ export const login = async (email: string, password: string) => {
   formData.append('username', email);
   formData.append('password', password);
 
-  const request = new Request('/api/token', {
+  const request = new Request(`${BACKEND_URL}/auth/token`, {
     method: 'POST',
     body: formData,
   });
@@ -47,66 +49,10 @@ export const login = async (email: string, password: string) => {
   }
 
   if ('access_token' in data) {
+    console.log({data})
     const decodedToken: any = decodeJwt(data['access_token']);
     localStorage.setItem('token', data['access_token']);
-    localStorage.setItem('permissions', decodedToken.permissions);
-  }
-
-  return data;
-};
-
-/**
- * Sign up via backend and store JSON web token on success
- *
- * @param email
- * @param password
- * @returns JSON data containing access token on success
- * @throws Error on http errors or failed attempts
- */
-export const signUp = async (
-  email: string,
-  password: string,
-  passwordConfirmation: string
-) => {
-  // Assert email or password or password confirmation is not empty
-  if (!(email.length > 0)) {
-    throw new Error('Email was not provided');
-  }
-  if (!(password.length > 0)) {
-    throw new Error('Password was not provided');
-  }
-  if (!(passwordConfirmation.length > 0)) {
-    throw new Error('Password confirmation was not provided');
-  }
-
-  const formData = new FormData();
-  // OAuth2 expects form data, not JSON data
-  formData.append('username', email);
-  formData.append('password', password);
-
-  const request = new Request('/api/signup', {
-    method: 'POST',
-    body: formData,
-  });
-
-  const response = await fetch(request);
-
-  if (response.status === 500) {
-    throw new Error('Internal server error');
-  }
-
-  const data = await response.json();
-  if (response.status > 400 && response.status < 500) {
-    if (data.detail) {
-      throw data.detail;
-    }
-    throw data;
-  }
-
-  if ('access_token' in data) {
-    const decodedToken: any = decodeJwt(data['access_token']);
-    localStorage.setItem('token', data['access_token']);
-    localStorage.setItem('permissions', decodedToken.permissions);
+    localStorage.setItem('token_type', data['token_type']);
   }
 
   return data;
@@ -114,5 +60,5 @@ export const signUp = async (
 
 export const logout = () => {
   localStorage.removeItem('token');
-  localStorage.removeItem('permissions');
+  localStorage.removeItem('token_type');
 };
