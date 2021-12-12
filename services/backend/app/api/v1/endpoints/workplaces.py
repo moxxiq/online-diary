918ad97934eea82d557771bd032e88c62c4145cd
Moxxiq <dimanavsisto@gmail.com>
2021-12-12 03:38:47 +0200

    Add endpoint to read all workplace details from student id

--- services/backend/app/api/v1/endpoints/workplaces.py
+++ services/backend/app/api/v1/endpoints/workplaces.py
@@ -18,7 +18,20 @@ async def create_workplace(payload: Workplace, current_user: UserInDB = Depends(
 
 @router.get("/{id}/", response_model=WorkplaceDB)
 async def read_workplace(id: int = Path(..., gt=0), user: UserInDB = Depends(get_current_user)):
+    """
+    Get a Workplace. It's the common of teacher, subject, class. Starting point to which tasks connect
+    :param id: id of the workplace needed
+    :param user: let only authorised users
+    :return: Awaitable Workplace with id of the teacher, subject, class
+    """
     workplace_in_db = await crud.workplaces.get(id)
     if not workplace_in_db:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Workplace not found")
     return workplace_in_db
+
+@router.get("/students/{student_id}/workplaces/detailed/")
+async def get_all_student_workplaces_with_details(student_id: int = Path(..., gt=0), current_user: UserInDB = Depends(get_current_user_with_scopes([1, 2, 3]))):
+    if current_user.get("type") not in [1, 2]:
+        if student_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Student is not allowed to see others work")
+    return await crud.workplaces.get_all_student_workplaces_with_details(student_id)
