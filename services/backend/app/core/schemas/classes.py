fc90ec7a2df7855d8679b3f65e40b20d8f6b89c1
Moxxiq <dimanavsisto@gmail.com>
2021-12-12 15:36:02 +0200

    Add class number default value

--- services/backend/app/core/schemas/classes.py
+++ services/backend/app/core/schemas/classes.py
@@ -1,11 +1,11 @@
-from datetime import date
+from datetime import datetime, date
 from typing import Optional
 
 from pydantic import BaseModel, Field
 
 class Class(BaseModel):
     name: str = Field(...)
-    number: int = Field(...)
+    number: int = datetime.now().year
 
 class ClassDB(Class):
     id: int = Field(..., gt=0)
