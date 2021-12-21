640f0eb6b3cd9ac0d0c00fae803047827fd90152
Moxxiq <dimanavsisto@gmail.com>
2021-12-21 09:44:36 +0200

    Method name refactor

--- services/backend/app/api/v1/endpoints/workplaces.py
+++ services/backend/app/api/v1/endpoints/workplaces.py
@@ -38,4 +38,4 @@ async def get_all_student_workplaces_with_details(student_id: int = Path(..., gt
 
 @router.get("/teachers/{teacher_id}/workplaces/detailed/")
 async def get_all_teachers_workplaces_with_details(teacher_id: int = Path(..., gt=0), current_user: UserWithID = Depends(get_current_user_with_scopes([1, 2]))):
-    return await crud.workplaces.get_all_teachers_workplaces_with_details(teacher_id)
+    return await crud.workplaces.get_all_teacher_workplaces_with_details(teacher_id)
